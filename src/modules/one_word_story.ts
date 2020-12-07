f11596375c92b0944f2c27452ccde40ebae77f60
SpaceEEC <spaceeec@yahoo.com>
2020-12-07 21:10:19 +0100

    feat(StoryModule): add cron to compile the story every Sunday

--- src/modules/one_word_story.ts
+++ src/modules/one_word_story.ts
@@ -1,5 +1,6 @@
-import { Message } from "discord.js";
-import { getConnection } from "typeorm";
+import { Message, MessageAttachment, TextChannel } from "discord.js";
+import { getConnection, MoreThanOrEqual } from "typeorm";
+import { schedule as scheduleCron } from "node-cron";
 
 import MessageEntity from "../entity/Message";
 import UserEntity from "../entity/User";
@@ -7,7 +8,8 @@ import { Module, IModuleConfig } from "../structures/Module";
 
 const { channel: channelId, enabled, eulaChannel } = require("../../config/story.json");
 
-export class StoryModule extends Module
+
+class OneWordStory extends Module
 {
 	public constructor(config: IModuleConfig)
 	{
@@ -17,6 +19,11 @@ export class StoryModule extends Module
 			eventName: "message",
 		});
 
+		if (enabled)
+		{
+			// At 00:00 every Sunday
+			scheduleCron("0 0 * * 0", this.compileStory.bind(this), { timezone: "Etc/UTC" });
+		}
 	}
 
 	public async handle(message: Message): Promise<void>
@@ -75,6 +82,48 @@ export class StoryModule extends Module
 
 		await message.reply("You have successfully accepted the EULA!");
 	}
+
+	private async compileStory(): Promise<void>
+	{
+		const trigger = new Date();
+		// Ensuring our date is actually at midnight.
+		trigger.setUTCHours(0, 0, 0, 0);
+		// If it's Monday already, go back to Sunday
+		trigger.setUTCDate(trigger.getUTCDate() - trigger.getUTCDay());
+
+		const weekAgo = new Date(trigger.valueOf() - 1000 * 60 * 60 * 24 * 7);
+
+		const messageRepo = getConnection().getRepository(MessageEntity);
+		const messages = await messageRepo.find({
+			order: {
+				createdAt: "ASC",
+			},
+			where: {
+				createdAt: MoreThanOrEqual(weekAgo),
+			},
+		});
+
+		if (!messages.length) return;
+
+		const content = messages.map(message => message.content).join(" ");
+
+		const message = await (this.client.channels.resolve(channelId) as TextChannel).send(
+			`Compiled story from ${this.formatDate(weekAgo)} until ${this.formatDate(trigger)}.`,
+			new MessageAttachment(Buffer.from(content, "utf8"), "story.txt"),
+		);
+
+		// TODO: handle max pins
+		await message.pin();
+	}
+
+	private formatDate(date: Date): string
+	{
+		return [
+			date.getUTCFullYear().toString().padStart(4, "0"),
+			(date.getUTCMonth() + 1).toString().padStart(2, "0"),
+			date.getUTCDate().toString().padStart(2, "0"),
+		].join("-");
+	}
 }
 
-export { StoryModule as Module };
+export { OneWordStory as Module };
