bd0c74365c6fa310a134b1a330b12cb5897dc33c
SpaceEEC <spaceeec@yahoo.com>
2020-12-19 17:50:15 +0100

    fix(index): log login failure

--- src/index.ts
+++ src/index.ts
@@ -25,4 +25,8 @@ client.on("ready", () => console.log("Ready", client.user!.tag));
 
 client
 	.login(token)
-	.catch(() => process.exit(1));
+	.catch((error) =>
+	{
+		console.error(error);
+		process.exit(1);
+	});
